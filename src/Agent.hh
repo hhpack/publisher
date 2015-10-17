99724ef2c0ffcc8453c49e2e673a70b1ad10f750
holyshared <holy.shared.design@gmail.com>
2015-10-17 21:33:40 +0900

    add file header

--- src/Agent.hh
+++ src/Agent.hh
@@ -1,5 +1,14 @@
 <?hh //strict
 
+/**
+ * This file is part of hhpack\publisher.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace hhpack\publisher;
 
 interface Agent<T>
