f498a0621d8c83789809da2a9e1d64a51860b647
holyshared <holy.shared.design@gmail.com>
2015-10-17 21:13:50 +0900

    fix member name

--- src/MessagePublicher.hh
+++ src/MessagePublicher.hh
@@ -5,31 +5,35 @@ namespace hhpack\publisher;
 final class MessagePublicher<T as Message> implements Publicher<T>
 {
 
+    private Vector<Agent<T>> $agents;
+
     public function __construct(
-        private Vector<Agent<T>> $subscribers = Vector {}
+        ?Traversable<Subscribable<T>> $subscribers
     )
     {
+        $mapper = ($subscriber) ==> new SubscribeAgent($subscriber);
+        $this->agents = Vector::fromItems($subscribers)->map($mapper);
     }
 
     public function registerSubscriber(Subscribable<T> $subscriber) : void
     {
-        $this->subscribers->add(new SubscribeAgent($subscriber));
+        $this->agents->add(new SubscribeAgent($subscriber));
     }
 
     public function unregisterSubscriber(Subscribable<T> $subscriber) : void
     {
-        foreach ($this->subscribers->getIterator() as $key => $agent) {
+        foreach ($this->agents->lazy() as $key => $agent) {
             if ($agent->matches($subscriber) === false) {
                 continue;
             }
-            $this->subscribers->removeKey($key);
+            $this->agents->removeKey($key);
         }
     }
 
     public function publish(T $message) : void
     {
-        foreach ($this->subscribers->items() as $subscriber) {
-            $subscriber->receive($message);
+        foreach ($this->agents->items() as $agent) {
+            $agent->receive($message);
         }
     }
 
