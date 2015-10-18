e4d79052a4e5d30fec251dbfa6634cf0c204b710
holyshared <holy.shared.design@gmail.com>
2015-10-18 16:07:55 +0900

    Update SubscribeAgent

--- src/SubscribeAgent.hh
+++ src/SubscribeAgent.hh
@@ -23,36 +23,15 @@ final class SubscribeAgent<T as Message> implements Agent<T>
         private Subscribable<T> $subscriber
     )
     {
-        $class = new ReflectionClass($subscriber);
-        $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
+        $methods = $this->receiverMethods($subscriber);
 
-        foreach ($methods as $method) {
-            $parameters = $method->getParameters();
-
-            if (count($parameters) <= 0) {
-                continue;
-            }
-
-            $parameter = $parameters[0];
-
-            $type = $parameter->getClass();
-
-            if ($type === null) {
-                continue;
-            }
-
-            $typeName = $type->getName();
-
-            if ($type->implementsInterface(Message::class) === false) {
-                continue;
-            }
-
+        foreach ($methods->lazy() as $typeName => $methodName) {
             $subscriptions = $this->subscriptions->get($typeName);
 
             if ($subscriptions === null) {
                 $subscriptions = Vector {};
             }
-            $subscriptions->add(new InvokeSubscription( Pair { $this->subscriber, $method->getName() }));
+            $subscriptions->add(new InvokeSubscription( Pair { $this->subscriber, $methodName }));
 
             $this->subscriptions->set($typeName, $subscriptions);
         }
@@ -75,6 +54,38 @@ final class SubscribeAgent<T as Message> implements Agent<T>
         foreach ($subscriptions->items() as $subscription) {
             $subscription->receive($message);
         }
+    }
+
+    private function receiverMethods(Subscribable<T> $subscriber) : ImmMap<string, string>
+    {
+        $class = new ReflectionClass($subscriber);
+        $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
+        $results = Map {};
+
+        foreach ($methods as $method) {
+            if ($method->getNumberOfRequiredParameters() <= 0) {
+                continue;
+            }
+
+            $parameters = $method->getParameters();
+            $parameter = $parameters[0];
+
+            $type = $parameter->getClass();
+
+            if ($type === null) {
+                continue;
+            }
+
+            $typeName = $type->getName();
+
+            if ($type->implementsInterface(Message::class) === false) {
+                continue;
+            }
+
+            $results->set($typeName, $method->getName());
+        }
+
+        return $results->toImmMap();
     }
 
 }
